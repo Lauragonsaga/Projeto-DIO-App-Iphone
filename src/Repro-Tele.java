dae44f52ae0ec39665420b1f59b4d9a361e1de3a
Laura Gonsaga <137855438+Lauragonsaga@users.noreply.github.com>
2023-10-18 14:29:50 -0300

    Update Repro-Tele.java

--- src/Repro-Tele.java
+++ src/Repro-Tele.java
@@ -1,1 +1,23 @@
+@@ -0,0 +1,10 @@
+pacote  aparelho . reproduzirmusical ;
 
+ interface  pública ReproduçãoIntfc {
+
+  vazio  tocar ();
+
+   pausa nula ();
+
+  void  selecionarMúsica ();
+}
+ 9 alterações: 9 adições e 0 exclusões9 
+orientacao-a-objetos-e-uml/src/aparelho/reprodutormusical/ReprodutorMusical.java
+@@ -0,0 +1,9 @@
+pacote  aparelho . reproduzirmusical ;
+
+public  class  ReprodutorMusical  estende  ReproduçãoAbst {
+
+  public  ReprodutorMusical ( String  nome ) {
+    super ( nome );
+  }
+
+}
